5af57f419eedf2d18e965b976707ae549cc6fdfd
Ben 10 <uncle@flash.co>
2025-06-09 15:36:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ async def login(page):
 
 async def main():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)  # debug mode
+        browser = await p.chromium.launch(headless=True)  # debug mode
         context = await browser.new_context(
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
             viewport={"width": 1280, "height": 800},
