c8c159c75bf98ece5c63817b7e0fd48bf187d5db
Ben 10 <uncle@flash.co>
2025-06-09 15:25:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ async def handle_group(page, thread_id, state):
 
 async def main():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)
+        browser = await p.chromium.launch(headless=True)
         context = await browser.new_context()
         page = await context.new_page()
 
